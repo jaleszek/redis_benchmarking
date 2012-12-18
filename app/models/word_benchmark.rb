3b679adedfd6dd5802cecc520a8f76f13245c59b
leszek <leszek@leszek-Lenovo-U310.(none)>
2012-12-18 18:46:53 +0100

    forcing overwrite data in db disabled

--- app/models/word_benchmark.rb
+++ app/models/word_benchmark.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
 class WordBenchmark
-  def self.bencz1(refresh_db = true)
+  def self.bencz1(refresh_db = false)
     Importers::Dictionaries::PwnWordnet.new.save if refresh_db
     Benchmark.bm do |x|
       x.report{
